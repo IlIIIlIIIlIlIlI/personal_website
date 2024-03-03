5fb24829c7eba113fd4057bc66c46d268cb6f483
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-03-03 22:35:25 +0530

    new changes

--- src/ExperinceChart/ExperinceChart.tsx
+++ src/ExperinceChart/ExperinceChart.tsx
@@ -7,46 +7,115 @@ function ExperinceChart() {
   const data: ApexOptions = {
     series: [
       {
+        name: 'Bob',
         data: [
           {
-            x: 'Analysis',
+            x: 'Design',
             y: [
-              new Date('2019-02-27').getTime(),
-              new Date('2019-03-04').getTime(),
+              new Date('2019-03-05').getTime(),
+              new Date('2019-03-08').getTime(),
             ],
-            fillColor: '#008FFB',
+          },
+          {
+            x: 'Code',
+            y: [
+              new Date('2019-03-02').getTime(),
+              new Date('2019-03-05').getTime(),
+            ],
+          },
+          {
+            x: 'Code',
+            y: [
+              new Date('2019-03-05').getTime(),
+              new Date('2019-03-07').getTime(),
+            ],
+          },
+          {
+            x: 'Test',
+            y: [
+              new Date('2019-03-03').getTime(),
+              new Date('2019-03-09').getTime(),
+            ],
+          },
+          {
+            x: 'Test',
+            y: [
+              new Date('2019-03-08').getTime(),
+              new Date('2019-03-11').getTime(),
+            ],
+          },
+          {
+            x: 'Validation',
+            y: [
+              new Date('2019-03-11').getTime(),
+              new Date('2019-03-16').getTime(),
+            ],
           },
           {
             x: 'Design',
             y: [
-              new Date('2019-03-04').getTime(),
-              new Date('2019-03-08').getTime(),
+              new Date('2019-03-01').getTime(),
+              new Date('2019-03-03').getTime(),
             ],
-            fillColor: '#00E396',
+          },
+        ],
+      },
+      {
+        name: 'Joe',
+        data: [
+          {
+            x: 'Design',
+            y: [
+              new Date('2019-03-02').getTime(),
+              new Date('2019-03-05').getTime(),
+            ],
           },
           {
-            x: 'Coding',
+            x: 'Test',
             y: [
+              new Date('2019-03-06').getTime(),
+              new Date('2019-03-16').getTime(),
+            ],
+          },
+          {
+            x: 'Code',
+            y: [
+              new Date('2019-03-03').getTime(),
               new Date('2019-03-07').getTime(),
-              new Date('2019-03-10').getTime(),
             ],
-            fillColor: '#775DD0',
-          },
-          {
-            x: 'Testing',
-            y: [
-              new Date('2019-03-08').getTime(),
-              new Date('2019-03-12').getTime(),
-            ],
-            fillColor: '#FEB019',
           },
           {
             x: 'Deployment',
             y: [
-              new Date('2019-03-12').getTime(),
+              new Date('2019-03-20').getTime(),
+              new Date('2019-03-22').getTime(),
+            ],
+          },
+          {
+            x: 'Design',
+            y: [
+              new Date('2019-03-10').getTime(),
+              new Date('2019-03-16').getTime(),
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Dan',
+        data: [
+          {
+            x: 'Code',
+            y: [
+              new Date('2019-03-10').getTime(),
               new Date('2019-03-17').getTime(),
             ],
-            fillColor: '#FF4560',
+          },
+          {
+            x: 'Validation',
+            y: [
+              new Date('2019-03-05').getTime(),
+              new Date('2019-03-09').getTime(),
+            ],
           },
         ],
       },
@@ -62,10 +131,14 @@ function ExperinceChart() {
       bar: {
         horizontal: true,
         distributed: true,
+        rangeBarGroupRows: true,
         dataLabels: {
           hideOverflowingLabels: false,
         },
       },
+    },
+    legend: {
+      show: false,
     },
     dataLabels: {
       enabled: true,
