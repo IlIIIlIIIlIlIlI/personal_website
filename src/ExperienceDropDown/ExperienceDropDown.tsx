d5629b81711e254f4e199df51fe69038d00e5f77
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-03-06 22:51:56 +0530

    new changes

--- src/ExperienceDropDown/ExperienceDropDown.tsx
+++ src/ExperienceDropDown/ExperienceDropDown.tsx
@@ -40,6 +40,36 @@ function ExperienceDropDown() {
             malesuada lacus ex, sit amet blandit leo lobortis eget.
           </AccordionDetails>
         </Accordion>
+
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <div className={`${scssObj.baseClass}__header-container`}>
+              <div className={`${scssSkillsObj.baseClass}__icon-container`}>
+                <PhonelinkIcon
+                  className={`${scssSkillsObj.baseClass}__icons`}
+                />
+              </div>
+
+              <div className={`${scssObj.baseClass}__content-container`}>
+                <div className={`${scssObj.baseClass}__designation`}>
+                  Full Stack Software Developer
+                </div>
+
+                <div className={`${scssObj.baseClass}__company`}>
+                  Aether Enterprise Applications LLP
+                </div>
+
+                <div className={`${scssObj.baseClass}__duration`}>
+                  Nov 2021 - Present
+                </div>
+              </div>
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
